bbc0ac3b4bdd0dffe4fb7ee873fb81620b2cbb72
lentqq <47202527+lentqq@users.noreply.github.com>
2019-07-14 19:26:59 +0300

    Add files via upload

--- 01.Rest Services And Ajax/Lab/01. JS-Applications-REST-Services-And-AJAX-Lab-Resources/04.XHR/scripts/app.js
+++ 01.Rest Services And Ajax/Lab/01. JS-Applications-REST-Services-And-AJAX-Lab-Resources/04.XHR/scripts/app.js
@@ -3,7 +3,7 @@ function loadRepos() {
    // req.onreadystatechange = function () {
    //    if (this.readyState === 4 && this.status === 200) {
    //       document.getElementById('res').textContent = this.responseText;
-         
+
    //    }
    // };
    // req.open('GET', 'https://swapi.co/api/people/1',true);
@@ -13,17 +13,20 @@ function loadRepos() {
    let url = `https://swapi.co/api/people/1`;
 
    fetch(url)
-   .then((response) => response.json())
-   .then((data) => {
-      document.body.style.backgroundImage = "url('640759.jpg')";
-      document.getElementById('info').innerHTML = '';
-      let person = Object.entries(data);
+      .then((response) => response.json())
+      .then((data) => {
+         document.body.style.backgroundImage = "url('640759.jpg')";
+         document.getElementById('info').innerHTML = '';
+         let person = Object.entries(data);
 
-      for (const [key, value] of person) {
-         let listItem = document.createElement('li');
-         listItem.textContent = `${key}: ${value}`;
-         document.getElementById('info').appendChild(listItem);
-         document.getElementById('info').style.color = 'yellow';
-      }
-   })
+         for (const [key, value] of person) {
+            let listItem = document.createElement('li');
+            listItem.textContent = `${key}: ${value}`;
+            // let pElement = document.createElement('p');
+            // listItem.innerHTML = <h1>`${key}: ${value}`</h1>;
+            // pElement.appendChild(listItem);
+            document.getElementById('info').appendChild(listItem);
+            document.getElementById('info').style.color = 'yellow';
+         }
+      })
 };
